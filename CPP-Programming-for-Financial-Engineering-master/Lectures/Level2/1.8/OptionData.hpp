dfda333487f050abb34d0aa069bb98a7a90fe6ae
Godwin-88 <godwinedgar@gmail.com>
2024-08-09 15:46:45 +0300

    c++

--- CPP-Programming-for-Financial-Engineering-master/Lectures/Level2/1.8/OptionData.hpp
+++ CPP-Programming-for-Financial-Engineering-master/Lectures/Level2/1.8/OptionData.hpp
@@ -14,21 +14,21 @@ using namespace std;
 struct OptionData
 { // Option data + behaviour
 
-	double K;
-	double T;
-	double r;
-	double sig;
+	double K;//Strike Price
+	double T;//Expiry Time
+	double r;//Interest rate
+	double sig;//Volatility
 
 	// Extra data 
 	double D;		// dividend
-	double beta;	// elasticity factor (CEV model)
+	double beta;	// elasticity factor (Constant Elasticity of Variance (CEV)  model)
 	
 	int type;		// 1 == call, -1 == put
 
 };
 
 void print (OptionData data) // Call by value
-{ 
+{ //his function prints the contents of an OptionData struct. It takes the struct as a parameter by value, meaning a copy of the struct is passed to the function.
 	cout << "Strike: " << data.K << endl;
 	cout << "Expiry: " << data.T << endl;
 	cout << "Interest: " << data.r << endl;
@@ -39,7 +39,8 @@ void print (OptionData data) // Call by value
 }
 
 void print2 (struct OptionData* data) // Call by value
-{ 
+{ //This function prints only some of the contents of an OptionData struct.
+// It takes a pointer to the struct as a parameter, allowing it to directly access the original struct rather than a copy.
 	cout << "Strike: " << data->K << endl;
 	cout << "Expiry: " << data->T << endl;
 /*	cout << "Interest: " << data.r << endl;
