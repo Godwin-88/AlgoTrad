40659b1b2ec8b13e1f4ada7ac604974f2f93b977
Godwin-88 <godwinedgar@gmail.com>
2024-08-13 16:21:30 +0300

    SUCCESSFUL DATA INSERTION FROM WIKEPEDIA

--- algorithmic_trading_book-master/sat_source/chapter7/retrieving_data.py
+++ algorithmic_trading_book-master/sat_source/chapter7/retrieving_data.py
@@ -3,35 +3,29 @@
 
 # retrieving_data.py
 
-from __future__ import print_function
-
 import pandas as pd
-#import MySQLdb as mdb
+from sqlalchemy import create_engine
 import pymysql
 
+# Database connection details
+db_host = 'localhost'
+db_user = 'sec_user'
+db_pass = '1120'
+db_name = 'securities_master'
 
-if __name__ == "__main__":
-    # Obtain a database connection to the MySQL instance
-    db_host = 'localhost'
-    db_user = 'sec_user'
-    db_pass = '1120'
-    db_name = 'securities_master'
-try:
-    con = pymysql.connect(host=db_host, user=db_user, password=db_pass, database=db_name)
-    print("Connected to the database successfully!")
-except pymysql.MySQLError as e:
-    print(f"Error {e.args[0]}: {e.args[1]}")
+# Create SQLAlchemy engine
+engine = create_engine(f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}")
 
-    # Select all of the historic Google adjusted close data
-    sql = """SELECT dp.price_date, dp.adj_close_price
-             FROM symbol AS sym
-             INNER JOIN daily_price AS dp
-             ON dp.symbol_id = sym.id
-             WHERE sym.ticker = 'GOOG'
-             ORDER BY dp.price_date ASC;"""
+# SQL query to get Google adjusted close prices
+sql = """SELECT dp.price_date, dp.adj_close_price
+         FROM symbol AS sym
+         INNER JOIN daily_price AS dp
+         ON dp.symbol_id = sym.id
+         WHERE sym.ticker = 'GOOG'
+         ORDER BY dp.price_date ASC;"""
 
-    # Create a pandas dataframe from the SQL query
-    goog = pd.read_sql_query(sql, con=con, index_col='price_date')    
+# Create a pandas dataframe from the SQL query
+goog = pd.read_sql_query(sql, con=engine, index_col='price_date')
 
-    # Output the dataframe tail
-    print(goog.tail())
+# Output the dataframe tail
+print(goog.head())
