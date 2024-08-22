07377f09707171db0a99f323830bee70ec22537f
Godwin-88 <godwinedgar@gmail.com>
2024-08-22 18:51:54 +0300

    binomial distribution in python

--- algorithmic_trading_book-master/aat_source/chapter-bayesian-statistics-intro-to-bayesian-methods/beta_binomial.py
+++ algorithmic_trading_book-master/aat_source/chapter-bayesian-statistics-intro-to-bayesian-methods/beta_binomial.py
@@ -1,48 +1,42 @@
 # beta_binomial.py
 
+
 import numpy as np
-from scipy import stats
-from matplotlib import pyplot as plt
+from scipy.stats import bernoulli, beta
+import matplotlib.pyplot as plt
 
+def plot_coin_toss_bayesian_update(number_of_trials, data):
+    """
+    Plot the Bayesian update of coin tosses using the Beta-Binomial model.
 
-if __name__ == "__main__":
-    # Create a list of the number of coin tosses ("Bernoulli trials")
-    number_of_trials = [0, 2, 10, 20, 50, 500]
-
-    # Conduct 500 coin tosses and output into a list of 0s and 1s
-    # where 0 represents a tail and 1 represents a head
-    data = stats.bernoulli.rvs(0.5, size=number_of_trials[-1])
-
-    # Discretise the x-axis into 100 separate plotting points
+    Parameters:
+    number_of_trials (list of int): List of the number of coin tosses to simulate.
+    data (numpy array): Array of coin toss results, where 0 represents tails and 1 represents heads.
+    """
     x = np.linspace(0, 1, 100)
 
-    # Loops over the number_of_trials list to continually add
-    # more coin toss data. For each new set of data, we update
-    # our (current) prior belief to be a new posterior. This is
-    # carried out using what is known as the Beta-Binomial model.
-    # For the time being, we won't worry about this too much.
+    fig, axes = plt.subplots(nrows=(len(number_of_trials) + 1) // 2, ncols=2, figsize=(10, 8))
+    axes = axes.flatten()
+
     for i, N in enumerate(number_of_trials):
-        # Accumulate the total number of heads for this
-        # particular Bayesian update
         heads = data[:N].sum()
+        ax = axes[i]
+        ax.set_title(f"{N} trials, {heads} heads")
 
-        # Create an axes subplot for each update
-        ax = plt.subplot(len(number_of_trials) / 2, 2, i + 1)
-        ax.set_title("%s trials, %s heads" % (N, heads))
-
-        # Add labels to both axes and hide labels on y-axis
-        plt.xlabel("$P(H)$, Probability of Heads")
-        plt.ylabel("Density")
+        ax.set_xlabel("$P(H)$, Probability of Heads")
+        ax.set_ylabel("Density")
         if i == 0:
-            plt.ylim([0.0, 2.0])
-        plt.setp(ax.get_yticklabels(), visible=False)
+            ax.set_ylim([0.0, 2.0])
+        ax.set_yticklabels([])
 
-        # Create and plot a Beta distribution to represent the
-        # posterior belief in fairness of the coin.
-        y = stats.beta.pdf(x, 1 + heads, 1 + N - heads)
-        plt.plot(x, y, label="observe %d tosses,\n %d heads" % (N, heads))
-        plt.fill_between(x, 0, y, color="#aaaadd", alpha=0.5)
+        y = beta.pdf(x, 1 + heads, 1 + N - heads)
+        ax.plot(x, y, label=f"observe {N} tosses,\n {heads} heads")
+        ax.fill_between(x, 0, y, color="#aaaadd", alpha=0.5)
 
-    # Expand plot to cover full width/height and show it
     plt.tight_layout()
     plt.show()
+
+if __name__ == "__main__":
+    number_of_trials = [0, 2, 10, 20, 50, 500]
+    data = bernoulli.rvs(0.5, size=number_of_trials[-1])
+    plot_coin_toss_bayesian_update(number_of_trials, data)
